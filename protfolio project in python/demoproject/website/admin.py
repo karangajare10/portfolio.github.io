01d46a1c23916f58c17fa1c5c484b055a6534b91
Karan Bajirao Gajare <gajarekaran84@gmail.com>
2024-12-07 21:40:58 +0530

    portfolio

---
+++ protfolio project in python/demoproject/website/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from website import models
+
+# Register your models here.
+admin.site.register(models.Nav)
+admin.site.register(models.Header)
+admin.site.register(models.About)
+admin.site.register(models.Happyclient)
+admin.site.register(models.Skill)
+# admin.site.register(models.Portfolio)
+admin.site.register(models.Services)
+admin.site.register(models. Testimonials)
+admin.site.register(models.Contact)
+
+
+
+
+
